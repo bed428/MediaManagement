92d8c83e951f4e18250b49935bf2016f51aa415d
bed428 <72625711+bed428@users.noreply.github.com>
2024-12-28 11:15:53 -0700

    Update OrganizeByCodecThenResolution.ps1
    Compressed functions to include more info depending on device type. Added metadata from Samsung, Sony

--- OrganizeByCodecThenResolution.ps1
+++ OrganizeByCodecThenResolution.ps1
@@ -1,69 +1,62 @@
-#Organizes video data by 'Video Format Video Frame Video Codec'
+#↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓EDIT THESE↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+    $TargetDirectory = "C:\Users\USERNAME\Videos\Folder01"
+    $ExifTool = "C:\Scripts\exiftool-13.10_64\exiftool.exe"
+
+    $VideoExtensions = @('.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.mpeg')
+    $RefFiles = Get-ChildItem -Path $TargetDirectory -File -Recurse | Where-Object { $VideoExtensions -contains $_.Extension }
+
+#↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑EDIT THESE↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
 
-$TargetDirectory = "C:\Users\USERNAME\Videos\Album01"
+#↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓No need to modify anything below↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
-$ExifTool = "C:\Scripts\exiftool-13.10_64\exiftool.exe"
+#By Default, ExifTool doesn't return a workable object, so we need to parse the output into a hashtable and then convert that to a PSCustomObject.
+  $ParsedObjects = @()
+  foreach($Item in $RefFiles){
+      $Output = &$ExifTool $Item.FullName
 
-#OPTIONAL - Remove comment on -recuse and it will dive into subfolders also.
-$RefFiles = Get-ChildItem -Path $TargetDirectory -File #-Recuse
-
-$ParsedObjects = @()
-foreach($Item in $RefFiles){
-    $Output = &$ExifTool $Item.FullName
-
-    # # Split the output into lines and Parse each line and create a hashtable
-    $Lines = $Output -split "`n"
-    $HashTable = @{}
-    foreach ($Line in $Lines) {
-        # Split on the first colon and trim whitespace
-        $Parts = $Line -split ":", 2
-        if ($Parts.Count -eq 2) {
-            $Key = $Parts[0].Trim()
-            $Value = $Parts[1].Trim()
-            $HashTable[$Key] = $Value
+      # #Split the output into lines and Parse each line and create a hashtable
+        $Lines = $Output -split "`n"
+        $HashTable = @{}
+        foreach ($Line in $Lines) {
+            # Split on the first colon and trim whitespace
+              $Parts = $Line -split ":", 2
+              if ($Parts.Count -eq 2) {
+                  $Key = $Parts[0].Trim()
+                  $Value = $Parts[1].Trim()
+                  $HashTable[$Key] = $Value
+              }
         }
-    }
 
-    # Convert the hashtable to a PSCustomObject
-    $ParsedObject = [PSCustomObject]$HashTable
-
-    $ParsedObjects += $ParsedObject
-}
-
-#Output Objects for line by line running/checking
-  #$ParsedObjects | select 'File Name','Video Format Video Frame Video Codec'
-
-#Items with a valid codec will have a folder created (if not already exists) and moved into the folder.
-  $ObjectsWithCodec = $ParsedObjects | where {$null -ne $_.'Video Format Video Frame Video Codec'}
-  foreach($item in $ObjectsWithCodec){
-      $ItemPath = ($item.Directory + "/" + $item.'file name')
-      $ItemCodec = ($Item.'Video Format Video Frame Video Codec')
-      
-      if( Test-Path ($TargetDirectory + '/' + $ItemCodec ){<#Do nothing#>}
-      else{ New-Item ($TargetDirectory + '/' + $ItemCodec) -ItemType Directory }
-      
-      try{
-        Move-Item -Path $ItemPath -Destination ($TargetDirectory + '\' + $Item.'Video Format Video Frame Video Codec') -ErrorAction Stop
-        Write-Host "Successfully moved $($Item.'File Name')"
-      }
-      catch{Write-Host -ForegroundColor Red $ItemPath 'failed!`n $($_.Exception)'  }
+      # Convert the hashtable to a PSCustomObject
+      $ParsedObject = [PSCustomObject]$HashTable
+      $ParsedObjects += $ParsedObject
   }
 
-#Determine which items do not have a codec and create a simple resolution folder for them, then move them there. 
-  $ObjectsWithOutCodec = $ParsedObjects | where {$null -eq $_.'Video Format Video Frame Video Codec'}
-  
-  foreach($Object in $ObjectsWithOutCodec){
-      $Resolution = $object.'Image Width' + 'x' + $object.'Image Height'
-      $ItemPath = ($Object.Directory + "/" + $Object.'file name')
+#Items with a valid codec will have a folder created (if not already exists) and moved into the folder.
+  foreach($Object in $ParsedObjects){
+      $DeviceModelName = 
+            if($Object.'Samsung Model'){$Object.'Samsung Model'} #Samsung devices. Tested with S21 Ultra
+            elseif($Object.'Device Model Name'){$Object.'Device Model Name'} #Sony devices. (Tested with A7C & A7IV)
+      $ObjectCodec = ($Object.'Video Format Video Frame Video Codec')
+      $Resolution = $Object.'Image Width' + 'x' + $Object.'Image Height'
+      $ObjectPath = ($Object.Directory + "/" + $Object.'file name')
+      $FrameRate = $Object.'Video Frame Rate'
+      $MajorBrand = 
+            if($object.'Major Brand' -like "*Sony*") {$Object.'Major Brand'} #AKA Like "Sony XAVC"
+            else{""} #For samsung devices it's an ISO# so we just want to ignore this. 
       
-      if(Test-Path ($TargetDirectory + '/' + $Resolution)){}
-      else{ New-Item ($TargetDirectory + '/' + $Resolution) -ItemType Directory}
-      
+      $ItemNewDirectoryName = $TargetDirectory + '\' + `
+        ($DeviceModelName + "_" -replace '^\s*_\s*', '') + `
+        ($MajorBrand + "_" -replace '^\s*_\s*', '') + `
+        ($ObjectCodec + "_" -replace '^\s*_\s*', '') + `
+        $Resolution + "_" + `
+        $FrameRate + "fps"
+
+      if( !(Test-Path $ItemNewDirectoryName) ){New-Item $ItemNewDirectoryName -ItemType Directory }
       try{
-        Move-Item -Path $ItemPath -Destination ($TargetDirectory + '/' + $Resolution) -ErrorAction Stop
-        Write-Host "Successfully moved $($Object.'File Name')"
+        Move-Item -Path $ObjectPath -Destination $ItemNewDirectoryName -ErrorAction Stop
+        Write-Host "Successfully moved $($Item.'File Name')"
       }
-      catch{ Write-Host -ForegroundColor Red $ItemPath 'failed!`n $($_.Exception)' }
-      
+      catch{Write-Host -ForegroundColor Red $ObjectPath 'failed!`n $($_.Exception)'  }
   }
